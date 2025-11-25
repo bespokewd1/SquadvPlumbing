4da65b80f9b2732365130e83ba62ceedde7a2ae9
churchilldotexe <churchillexe@gmail.com>
2025-11-25 19:44:29 +0800

    chore: updated client data and og

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,20 +1,20 @@
 module.exports = {
-    "name": "Montano Plumbing & Contracting LTD.",
-    "email": "info@montanoplumbing.ca",
-    "phoneForTel": "",
-    "phoneFormatted": "780-708-5345",
-    "address": {
-        "lineOne": "",
-        "lineTwo": "",
-        "city": "Edmonton, Alberta Canada",
-        "state": "",
-        "zip": "",
-        "mapLink": "https://maps.app.goo.gl/N5znB9zQ7S7DxkoWA"
-    },
-    "socials": {
-        "facebook": "https://www.facebook.com/yeg.mpc/",
-        "instagram": "https://www.instagram.com/",
-    },
-    //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    "domain": "https://www.example.com"
+  "name": "Squad V Plumbing Inc.",
+  "email": "SQUADV726@gmail.com",
+  "phoneForTel": "7806677169",
+  "phoneFormatted": "(780) 667-7169",
+  "address": {
+    "lineOne": "432 Dunluce Rd. NW",
+    "lineTwo": "",
+    "city": "Edmonton",
+    "state": "AB",
+    "zip": "",
+    "mapLink": "https://maps.app.goo.gl/VhKQQrnjWs6opbma6"
+  },
+  "socials": {
+    "facebook": "https://www.facebook.com/profile.php?id=100063951556487",
+    "instagram": "#",
+  },
+  //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
+  "domain": "https://squadvplumbing.ca"
 };
